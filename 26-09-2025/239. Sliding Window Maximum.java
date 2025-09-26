3440849ff8f50851638ed327de3969ce95cb9585
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2025-09-26 23:05:30 +0530

    Update 239. Sliding Window Maximum.java

--- 26-09-2025/239. Sliding Window Maximum.java
+++ 26-09-2025/239. Sliding Window Maximum.java
@@ -3,26 +3,17 @@ import java.util.Deque;
 
 class Solution {
   public int[] maxSlidingWindow(int[] nums, int k) {
-    if (nums == null || nums.length == 0 || k == 0)
-      return new int[0];
+    int[] ans = new int[nums.length - k + 1];
+    Deque<Integer> max = new ArrayDeque<>();
 
-    int n = nums.length;
-    int[] ans = new int[n - k + 1];
-    Deque<Integer> dq = new ArrayDeque<>(); // store indices
-
-    for (int i = 0; i < n; i++) {
-      while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
-        dq.pollFirst();
-      }
-      while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-        dq.pollLast();
-      }
-
-      dq.offerLast(i);
-
-      if (i >= k - 1) {
-        ans[i - k + 1] = nums[dq.peekFirst()];
-      }
+    for (int i = 0; i < nums.length; ++i) {
+      while (!max.isEmpty() && max.peekLast() < nums[i])
+        max.pollLast();
+      max.offerLast(nums[i]);
+      if (i >= k && nums[i - k] == max.peekFirst()) // out-of-bounds
+        max.pollFirst();
+      if (i >= k - 1)
+        ans[i - k + 1] = max.peekFirst();
     }
 
     return ans;
@@ -32,6 +23,7 @@ class Solution {
 
 //Time Complexity:O(n)
 //Space Complexity:O(n)
+
 
 
 
