25653ef1ceb483c2bc4db4ff538784e0ae15051d
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2025-10-05 10:53:21 +0530

    Add files via upload

---
+++ 04-10-2025/53. Maximum Subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return subarray(nums);
+    }
+    public static int subarray(int []arr) {
+		int ans=Integer.MIN_VALUE;
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum+arr[i];
+			ans=Math.max(ans,sum);
+			if(sum<0) {
+				sum=0;
+			}
+		}
+		return ans;
+		
+	}
+}
